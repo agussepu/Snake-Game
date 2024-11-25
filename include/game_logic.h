1e306016444472e5678a103da7053609decdabb4
agus <elagussepulveda@gmail.com>
2024-11-25 17:54:21 -0300

    Ya casi esta listo

--- include/game_logic.h
+++ include/game_logic.h
@@ -1,4 +1,8 @@
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
 #include "graphics.h"
+
 #define SNAKE_SPEED 120
 
 //     [ Funciones ] 
@@ -8,3 +12,5 @@ void input(int *running, int *dir_x, int *dir_y);
 Point generate_food(Point *snake, int length);
 int check_collision(Point *snake, int length, int grid_width, int grid_height);
 int checkFinish();
+
+#endif // GAME_LOGIC_H
